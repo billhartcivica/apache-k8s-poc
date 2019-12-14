6912d2b1dd92f5d170bcea383d388270652d7f79
Bill Hart <32137104+billhartcivica@users.noreply.github.com>
2019-12-14 16:19:49 +0000

    Update push.sh

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@
 # Script for loggin into Dockerhub and pushing image to repo
 export VERSION=`cat version.txt`
 echo "$DOCKER_PW" | docker login -u "$DOCKER_LOGIN" --password-stdin
-docker tag local/apachetest:$VERSION crownpeak/apachetest:$VERSION
-docker push crownpeak/apachetest:$VERSION
+docker tag local/apachetest:$VERSION wtghcivica/apachetest:$VERSION
+docker push wtghcivica/apachetest:$VERSION
